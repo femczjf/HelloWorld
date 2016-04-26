bf8ee7ecc3679e90b86e5d7214809f2e122ba112
Sandra <2233603700@qq.com>
2016-04-26 14:13:05 +0800

    Initial commit

---
+++ src/com/youneek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.youneek.demo;
+
+/**
+ * Created by user on 2016/4/26.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world hello");
+    }
+}
